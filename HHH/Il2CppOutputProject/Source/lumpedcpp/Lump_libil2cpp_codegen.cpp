d245c234df26c1858aad3d380fad02d834ebd1a1
Jeonghyun109 <kjhrlawjdgus@naver.com>
2021-07-20 12:10:30 +0900

    Delete build folder of Unity

--- HHH/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\HCIL\depth_funneling\HHH\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\HCIL\depth_funneling\HHH\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\HCIL\depth_funneling\HHH\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
